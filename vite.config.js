690701cdde38f74f91ad4d02743888e1d57b6280
John <john.26sfj@gmail.com>
2026-03-12 10:25:19 +0100

    trying for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
-  base: "/portfolio-manager/", // Replace <YOUR_REPO_NAME> with your actual repository name
-  // ... other configurations
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
+
+// export default defineConfig({
+//   base: "/portfolio-manager/", // Replace <YOUR_REPO_NAME> with your actual repository name
+//   // ... other configurations
+// });
